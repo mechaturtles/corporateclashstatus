9f4f1beeb906f8d11b540bf384e7f38093c41621
mechaturtles <bryce.christian@gmail.com>
2020-05-21 23:40:34 -0500

    Removed / from image source directories because Github doesn't like that.

--- js/district.js
+++ js/district.js
@@ -12,12 +12,12 @@ const loadDistrict = (obj, index, district) => {
 
 	if (obj.invasion_online) {
 		let cogName = obj.cogs_attacking.replace(/ /g,"_");
-		var icon = `<div class="icon"> <img src = "/images/${cogName}.png" alt = "${obj.cogs_attacking}" align = "center" onerror="this.src='/images/FlippedFlippy.png'"/> </div>`;
+		var icon = `<div class="icon"> <img src = "images/${cogName}.png" alt = "${obj.cogs_attacking}" align = "center" onerror="this.src='images/FlippedFlippy.png'"/> </div>`;
 		text += `<p style="color: red;"> <b>${obj.name}</b> is being attacked by <b>${obj.cogs_attacking}</b> cogs! </p>`;
 		
 	}
 	else {
-		var icon = `<div class="icon"> <img src = "/images/Flippy.png" alt = "Flippy" align = "center"/> </div>`;
+		var icon = `<div class="icon"> <img src = "images/Flippy.png" alt = "Flippy" align = "center"/> </div>`;
 		text += `<p style="color: green;"> <b>${obj.name}</b> is currently safe!</p>`;
 	}
 	text += `<p> There are currently <b>${obj.population}</b> Toons defending this district. </p>`;
