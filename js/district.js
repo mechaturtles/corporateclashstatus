87b41630af9245353f2ce6408d57d222e957309a
mechaturtles <bryce.christian@gmail.com>
2020-05-23 15:40:49 -0500

    Added auto refresh functionality and disabled caching for API on browsers.
    XMLHttpRequest was ditched in favor of Fetch.
    
    Every 60s, district data is requested from the API. Every time the timer for an invasion ends, a request is also made after 5s.
    
    District element ids were changed to their names so that elements could be replaced instead of reloading the webpage.

--- js/district.js
+++ js/district.js
@@ -1,20 +1,27 @@
-let request = new XMLHttpRequest();
-request.open('get', 'https://corporateclash.net/api/v1/districts.js');
-//request.open('get', 'https://mechaturtles.com/corporateclashstatus/js/testdistrict.js');
-request.send();
+const link = "https://corporateclash.net/api/v1/districts.js"
+//const link = "https://mechaturtles.com/corporateclashstatus/js/testdistrict.js" //Test Link
+const requestDistricts = link => fetch(link, { cache: "no-store" })
+	.then(response => response.json())
+	.then(data => data.forEach(loadDistrict));
+
+requestDistricts(link);
+setInterval(() => requestDistricts(link), 60000);
 
 const loadDistrict = (data, index, district) => {
 	let div = document.createElement("div")
-	div.className = "district";
-	div.id = "district_" + index;
-	
+
 	let text = "";
 
+	div.className = "district";
+	div.id = data.name.replace(/ /g, "_");
+
+
+
 	if (data.invasion_online) {
-		let cogName = data.cogs_attacking.replace(/ /g,"_");
+		let cogName = data.cogs_attacking.replace(/ /g, "_");
 		var icon = `<div class="icon"> <img src = "images/${cogName}.png" alt = "${data.cogs_attacking}" align = "center" onerror="this.src='images/Unknown.png'"/> </div>`;
 		text += `<p style="color: red;"> <b>${data.name}</b> is being attacked by <b>${data.cogs_attacking}</b> cogs! </p>`;
-		
+
 		var countID = `timer_${index}`
 		var countdown = `<div class="countdown"><div><p>Remaining Time</p><h1 id=${countID}></h1></div></div>`
 		// const endTime = new Date(); //Testing purposes only
@@ -30,15 +37,20 @@ const loadDistrict = (data, index, district) => {
 	text += `<p> There are currently <b>${data.population}</b> Toons defending this district. </p>`;
 	text = `<div class="details"> ${text} </div>`;
 
-
-
 	div.innerHTML = icon + text + countdown;
 
 	div.style.cursor = "pointer";
 	div.onclick = () => {
 		loadDistrictModal(data);
 	};
-	document.getElementById("districtList").appendChild(div);
+
+	let currentDiv = document.getElementById(div.id);
+	if (currentDiv) {
+		document.getElementById("districtList").replaceChild(div, currentDiv);
+	}
+	else {
+		document.getElementById("districtList").appendChild(div);
+	};
 };
 
 const loadDistrictModal = (data) => {
@@ -46,23 +58,25 @@ const loadDistrictModal = (data) => {
 	modal.style.display = "block";
 
 	modal.onclick = (event) => {
-		if (event.target == modal) {
+		if (event.target === modal) {
 			modal.style.display = "none";
 			clearInterval(modalTimer);
 		}
 	}
 
+
+
 	let modalBox = document.createElement("div");
 	modalBox.className = "modalBox";
-	
+
 
 	let text = "";
 
 	if (data.invasion_online) {
-		let cogName = data.cogs_attacking.replace(/ /g,"_");
+		let cogName = data.cogs_attacking.replace(/ /g, "_");
 		var icon = `<div class="icon"> <img src = "images/${cogName}.png" alt = "${data.cogs_attacking}" align = "center" onerror="this.src='images/Unknown.png'"/> </div>`;
 		text += `<p style="color: red;"> <b>${data.name}</b> is being attacked by <b>${data.cogs_attacking}</b> cogs! </p>`;
-		
+
 		var defeat = `<p> <b>${data.count_defeated}</b> out of <b>${data.count_total}</b> cogs have been defeated during this invasion. </p>`;
 
 		var countID = `modalTimer`
@@ -86,11 +100,7 @@ const loadDistrictModal = (data) => {
 	modal.innerHTML = modalBox.outerHTML;
 }
 
-request.onload = function() {
-	
-	let ccData = JSON.parse(request.response);
-	ccData.forEach(loadDistrict);
-};
+
 
 
 /** Used to perform a countdown for each item
@@ -103,11 +113,12 @@ const timer = (id, endTime) => {
 		let timeLeft = endTime - currentTime;
 
 		let timeString = new Date(timeLeft).toISOString();
-		document.getElementById(id).innerHTML = timeString.slice(11,19);
+		document.getElementById(id).innerHTML = timeString.slice(11, 19);
 
-		if(timeLeft < 0 ) {
+		if (timeLeft < 0) {
 			clearInterval(interval);
 			document.getElementById(id).innerHTML = "OVER";
+			setTimeout(() => requestDistricts(link), 5000);
 		}
 	}, 100);
 	return interval;
