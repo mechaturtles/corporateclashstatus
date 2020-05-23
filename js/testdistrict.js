376a41659935cd48fe1c6664a8cfec76c72e90c9
mechaturtles <bryce.christian@gmail.com>
2020-05-23 00:47:52 -0500

    Another test update.

--- js/testdistrict.js
+++ js/testdistrict.js
@@ -1,4 +1,4 @@
 [{"name":"Bamboozle Bay","online":true,"population":53,"invasion_online":false,"last_update":1590104527,"cogs_attacking":"None","count_defeated":625,"count_total":0,"remaining_time":0},
-{"name":"Comical Canyon","online":true,"population":43,"invasion_online":true,"last_update":,"cogs_attacking":"Big Fish","count_defeated":54,"count_total":3745,"remaining_time":15},
+{"name":"Comical Canyon","online":true,"population":43,"invasion_online":true,"last_update":"","cogs_attacking":"Big Fish","count_defeated":54,"count_total":3745,"remaining_time":15},
 {"name":"Kazoo Kanyon","online":true,"population":53,"invasion_online":false,"last_update":1590104530,"cogs_attacking":"None","count_defeated":4928,"count_total":0,"remaining_time":0},
 {"name":"Foghorn Falls","online":true,"population":54,"invasion_online":true,"last_update":1590212611,"cogs_attacking":"Legal Eagle","count_defeated":343,"count_total":1001,"remaining_time":180}]
